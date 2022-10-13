8d89683e87e6d4ca057414ff10094c0ee0f9ef70
facadosj <jeremiahfacados@gmail.com>
2022-10-13 13:05:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio>
-#include <unistd.h>
+#include <stdio.h>
 /**
- * main - Entry
- * Return: 1
+ * main - program entry point
+ * Return: 1 on success;
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
